63fdc13b09e02418a55be2010db61e0d946642d5
ragtam <mag190033@gmail.com>
2020-03-29 13:43:19 +0200

    app copied from v8 version, bootstrap installed

--- projects/yt-player-angular-app/src/app/app.module.ts
+++ projects/yt-player-angular-app/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { YtPlayerAngularModule } from 'yt-player-angular';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    YtPlayerAngularModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
